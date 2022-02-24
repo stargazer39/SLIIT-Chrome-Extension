853a056065c3c316295369579e0dd987cafdd000
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-02-24 10:58:55 +0530

    Hide Old modules implementation

--- src/tsx/contentScript.tsx
+++ src/tsx/contentScript.tsx
@@ -1,5 +1,37 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if(msg.bgColor) {
-        document.body.style.backgroundColor = msg.bgColor;
+import $ from "jquery";
+
+chrome.storage.sync.get('blocked_modules', (res) => {
+    if(res.blocked_modules){
+        for(const m of res.blocked_modules.modules){
+            $(`a[title="${m}"]`).css({
+                display: "none",
+            });
+            console.log(m);
+        }
     }
 })
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    console.log(msg);
+    switch(msg.command){
+        case "GET_TABS":
+            sendResponse({
+                modules: getTabs(),
+            })
+    }
+})
+
+function getTabs(): string[]{
+    const modules = $("a[title='My courses'] ~ ul > li a");
+
+    const site_array : string[] = [];
+
+    for(let m of modules){
+        const title = $(m).attr("title"); 
+        if(!title)
+            continue
+            
+        site_array.push(title);   
+    }
+
+    return site_array;
+}
--- src/tsx/popup.tsx
+++ src/tsx/popup.tsx
@@ -1,39 +1,120 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom'
 
-const PopupHome = () => {
-    const [bgColor, setBgColor] = useState<string>('red');
+type Module = {
+    active: boolean,
+    title: string
+}
+
+function PopupHome(){
+    const [modules, setModules] = useState<Module[]>([]);
 
     useEffect(() => {
-        // Get old preference form chrome.storage
-        chrome.storage.sync.get(['bgColor'], (res) => {
-            setBgColor(res.bgColor);
-        });
-    });
+        (async function() {
+            // Get current sites from the site
+            let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    const changeBodyBg = () => {
-        document.body.style.backgroundColor = bgColor;
-        console.log(bgColor);
-    }
+            if(!tab.id)
+                throw new Error("No tab id");
+            
+            chrome.tabs.sendMessage(tab.id, {
+                command: "GET_TABS",
+            },(resp) => {
+                let new_modules : Module[] = resp.modules.map((val : string) => {
+                    return { active: false, title: val }
+                })
 
-    const changeBackground = async () => {
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        console.log(`Tab ID ${tab.id} ${bgColor}`);
+                // setModules(new_modules);
+                 // Get old preference form chrome.storage
+                chrome.storage.sync.get('blocked_modules', (res) => {
+                    if(!res.blocked_modules){
+                        chrome.storage.sync.set({ 'blocked_modules': {
+                            modules: [],
+                        }})
+                        console.log(res, "Reset");
+                        return;
+                    }
+                    console.log(res);
+                    const mod = res.blocked_modules.modules as string[];
+                    const modmap = new Map<string, boolean>();
 
-        chrome.tabs.sendMessage(tab.id!, {
-            bgColor: bgColor
-        });
-    }
+                    mod.forEach((val) => { modmap.set(val, true);});
+                    console.log(modmap)
+                    if(mod){
+                        let enabled : Module[] = [];
+                        let disabled : Module[] = [];
+
+                        new_modules.forEach(element => {
+                            if(modmap.has(element.title)){
+                                element.active = true;
+                                
+                                enabled.push(element);
+                                return;
+                            }
+                            disabled.push(element);
+                        });
+                        
+                        setModules([...enabled, ...disabled]);
+                    }
+                });
+            });
+        })()
+    }, []);
 
     return (
         <>
-            <h1> DOM renders </h1>
-            <div>Current bgcolor is {bgColor}</div>
-            <input type="button" onClick={changeBackground} value="Change Bg"></input>
+            <h1>Modules</h1>
+            <div>
+                {
+                    modules.map((value, index) => {
+                        return <ModuleDisplay module={value} key={index}/>
+                    })
+                }
+            </div>
         </>
     )
 }
 
+type ModulesProps = {
+    module: Module
+}
+
+function ModuleDisplay(props : ModulesProps){
+    const [active, setActive] = useState<boolean>(props.module.active);
+
+    const toggleActive = () => {
+       chrome.storage.sync.get('blocked_modules', (data) => {
+            let idx = data.blocked_modules.modules.indexOf(props.module.title);
+            let new_data : string[] = [];
+            console.log(data);
+            if(idx != -1){
+                data.blocked_modules.modules.splice(idx, 1)
+                
+                chrome.storage.sync.set(data, () => {
+                    setActive(false);
+                });
+            }else{
+                new_data.push(...data.blocked_modules.modules, props.module.title);
+                data.blocked_modules.modules = new_data;
+
+                chrome.storage.sync.set(data, () => {
+                    setActive(true);
+                });
+            }
+       })
+    }
+
+    return (
+        <div>
+            {props.module.title} 
+            <button onClick={toggleActive}>{active ? "Enable" : "Disable"}</button>
+        </div>
+    )
+}
+
+// async function sendToTab(obj : any){
+    
+// }
 ReactDOM.render(
     <PopupHome/>,
     document.getElementById('root')
